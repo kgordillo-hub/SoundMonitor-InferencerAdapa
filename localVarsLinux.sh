cce055263c5b2e4f305c165bcbbee6aad5f0b7f0
alienx456 <estebanelias27dev@gmail.com>
2020-09-29 23:01:41 -0500

    Tests integration with S3

--- localVarsLinux.sh
+++
@@ -1,7 +0,0 @@
-export DATA_PATH=/home/esteban/Documentos/Tesis/repos/AM-inferenciatorKafka/data/
-export MODEL_URL=https://github.com/sainathadapa/dcase2019-task5-urban-sound-tagging/releases/download/1.0/model_system1
-export MODEL_NAME=model_system1
-export DEVICE_NAME=cpu
-export AUDIO_PATH=/home/esteban/Documentos/Tesis/audio_test/
-export CHANNEL_MEANS_FILE=channel_means.npy
-export CHANNEL_STDS_FILE=channel_stds.npy
--- mainTest.py
+++ mainTest.py
@@ -1,18 +1,26 @@
 from inferencer.Adapatask5 import DcaseAdapatask5
-import base64
 import io
 import soundfile as sf
 import os
+import boto3
+
+session = boto3.Session(
+    aws_access_key_id=os.environ['AWS_KEY'],
+    aws_secret_access_key=os.environ['AWS_SECRET']
+)
+
+s3 = session.resource('s3')
+
+bucket = s3.Bucket('sistemamonitoreoacustico')
+object = bucket.Object('00_000066.wav')
+audioStream = io.BytesIO()
+object.download_fileobj(audioStream)
 
 inferencer = DcaseAdapatask5()
 
-audio = open(os.environ['TEST_AUDIO_FILE'], 'rb').read()
+audio = audioStream.getvalue()
 
-audioBase64 = base64.b64encode(audio)
-
-audioDecoded = base64.decodebytes(audioBase64)
-
-data, samplerate = sf.read(io.BytesIO(audioDecoded))
+data, samplerate = sf.read(io.BytesIO(audio))
 
 result = inferencer.runInferencer(data, samplerate)
 
