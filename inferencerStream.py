49ac1913bab26efa715854b6d944895ecc06acec
alienx456 <estebanelias27dev@gmail.com>
2020-10-12 16:15:37 -0500

    Added producer, using env vars for constant values

--- inferencerStream.py
+++ inferencerStream.py
@@ -1,4 +1,5 @@
-from kafka import KafkaConsumer
+from kafka import KafkaConsumer, KafkaProducer
+from json import dumps
 import io
 import soundfile as sf
 from inferencer.Adapatask5 import DcaseAdapatask5
@@ -10,19 +11,23 @@ logging.getLogger().setLevel(logging.INFO)
 inferencer = DcaseAdapatask5()
 awsS3 = AwsS3Resource()
 
-consumer = KafkaConsumer('audio-upload-event',
-                         group_id='inferencer-group',
+consumer = KafkaConsumer(os.environ['AUDIO_UPLOAD_EVENT'],
+                         group_id=os.environ['GROUP_ID'],
                          bootstrap_servers=[os.environ['KAFKA_SERVER']],
                          auto_offset_reset='earliest')
+producer = KafkaProducer(bootstrap_servers=[os.environ['KAFKA_SERVER']],
+                         value_serializer=lambda x: dumps(x).encode(os.environ['ENCODE_FORMAT']))
 
 
 for message in consumer:
-    fileName = message.value.decode('utf-8')
+    fileName = message.value.decode(os.environ['ENCODE_FORMAT'])
     logging.info("New Audio arrived ID {}".format(fileName))
     try:
         data, samplerate = sf.read(io.BytesIO(awsS3.getStreamData(fileName)))
         result = inferencer.runInferencer(fileName, data, samplerate)
-        print(result)
         logging.info("Processing Finished for {}".format(fileName))
+        logging.info("Sending result file:{} to topic inference-event".format(fileName))
+        producer.send(os.environ['INFERENCE_EVENT'], value=result.to_json())
+        logging.info("{} Jobs Finished".format(fileName))
     except Exception as e:
         logging.error('There was an error while Processing : {}'.format(str(e)))
