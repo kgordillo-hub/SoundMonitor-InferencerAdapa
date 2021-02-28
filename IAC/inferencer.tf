309df6c8572c21475b88b3477e2a9892ab3e3346
alienx456 <estebanelias27dev@gmail.com>
2021-02-28 15:32:03 -0500

    Deploying using module

---
+++ IAC/inferencer.tf
@@ -0,0 +1,30 @@
+module "inferencer" {
+  source = "https://github.com/AlienX456/SoundMonitor-IAC-Infrastructure-Common/tree/master/inferencer"
+  /*
+  family-name:""
+  cluster-name:""
+  cluster-arn:""
+  service-name:""
+  sound-monitor-subnet:""
+  cpu:""
+  memory:""
+
+  aws_region:"us-east-1"
+  aws_provider_key:""
+  aws_provider_secret:""
+  aws_inferencer_key:""
+  aws_inferencer_secret:""
+
+  kafka_group_id:""
+  kafka_audio_upload_event:""
+  kafka_group_id:""
+  kafka_audio_upload_event:""
+  kafka_inference_event:""
+  kafka_encode_format:"UTF-8"
+
+  ecr_image_tag:""
+  ecr_image_repo:""
+  records_bucket_name:""
+  kafka_server:""
+  */
+}
