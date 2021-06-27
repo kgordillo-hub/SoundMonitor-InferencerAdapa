4de232655d058de4880fabb2523b8486eca1713b
alienx456 <estebanelias27dev@gmail.com>
2021-06-27 13:13:09 -0500

    deployment fix

--- IAC/inferencer.tf
+++ IAC/inferencer.tf
@@ -5,6 +5,7 @@ module "inferencer" {
   family-name = "monitor-inferencer-adapa"
   cpu= "256"
   memory= "512"
+  number_of_tasks="1"
 
   aws_provider_key= var.aws_provider_key
   aws_provider_secret= var.aws_provider_secret
