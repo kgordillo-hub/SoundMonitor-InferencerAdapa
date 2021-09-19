f0304b6abd983717cad8e077d72366aa7044dc21
alienx456 <estebanelias27dev@gmail.com>
2021-09-19 11:37:22 -0500

    prepare to use new infrastructure version

--- IAC/backend.tf
+++ IAC/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket  = "terraform-monitor-provide-states-files"
-    key     = "adapa-v2.tfstate"
-    region  = "us-east-1"
+    bucket = "terraform-monitor-provide-states-files"
+    key    = "adapa-v2.tfstate"
+    region = "us-east-1"
   }
 }
