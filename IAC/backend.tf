2b7c832f4387c1c92dbe23777ac11db3d98a9b6d
kgordillo-hub <ksgordilloo@correo.udistrital.edu.co>
2022-12-02 09:59:16 -0500

    Update backend.tf

--- IAC/backend.tf
+++ IAC/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-monitor-provide-states-files"
+    bucket = "terraform-monitor-provide-states-files-03"
     key    = "adapa-v2.tfstate"
     region = "us-east-1"
   }
